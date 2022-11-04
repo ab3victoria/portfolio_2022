6fdb2bf0e0481706fec77caf94f7a8ca727cafb6
ab3victoria <ab3victoria@gmail.com>
2022-11-04 15:28:15 +0200

    added cocktails project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -142,6 +142,31 @@ const Work = () => {
               </div>
             </div>
           </div>
+           {/* Grid Item */}
+           <div
+            style={{ backgroundImage: `url(https://i.postimg.cc/FsLywRdR/cocktails.png)` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Cocktails Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a target="_blank" href='https://cocktails-app-react.firebaseapp.com/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Website
+                  </button>
+                </a>
+                <a href='https://github.com/ab3victoria/cocktails-project'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+          
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
             {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(https://i.postimg.cc/pXNxDwF6/groceryimg.png)` }}
